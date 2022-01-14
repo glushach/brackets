ae000dda1e76bd7ad88f2523af9439233a3e5375
Alex <shoppingplaneta@gmail.com>
2022-01-14 10:31:16 +0200

    done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arrWithBrackets = bracketsConfig.map((item) => item.reduce((a, v) => a + v)); //превратить матрицу в массив строк
+
+    let compareLength = str.length; //длина строки после удаления совпадений в цикле
+
+    while (str.length) {
+        arrWithBrackets.forEach((value) => {
+            str = str.replace(value, '') //удалить вcе совпадения со строки, которые совпадает с value
+        });
+
+        if (compareLength === str.length) return false; //если в forEach не найдено совпадение, return прервет цикл
+
+        compareLength = str.length;
+    }
+
+    return true; //строка пустая
+
 }
